5b715c7aa71511419fd51d4257cfc0aef49f405b
mghpt <mghpt@MEGRONPC>
2026-04-22 14:46:57 -0600

    Add project files.

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Mvc;
+using MyProfile.Models;
+
+namespace MyProfile.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(string name, string email, string subject, string message)
+        {
+            try
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+                var smtp = new SmtpClient("smtp.mail.yahoo.com", 587)
+                {
+                    EnableSsl = true,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential("mghptl@yahoo.com", "CenSait@1115"),
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    Timeout = 20000
+                };
+
+                var mail = new MailMessage
+                {
+                    From = new MailAddress("mghptl@yahoo.com"),
+                    Subject = subject,
+                    Body = $"Name: {name}\nEmail: {email}\nMessage: {message}"
+                };
+
+                mail.To.Add("your@yahoo.com");
+
+                smtp.Send(mail);
+
+                return Content("Message sent successfully!");
+            }
+            catch (Exception ex)
+            {
+                return Content(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Models/Project.cs
@@ -0,0 +1,11 @@
+﻿namespace MyProfile.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string TechStack { get; set; }
+        public string GitHubLink { get; set; }
+    }
+}
